62b09b4c71187de64a6a8ff033bb202ca1585912
bsanty-santhosh <89247020+bsanty-santhosh@users.noreply.github.com>
2022-03-25 14:57:42 +0530

    header layout fix

--- src/Layout.js
+++ src/Layout.js
@@ -19,53 +19,70 @@ function Layout() {
     document.body.removeChild(downloadLink);
   };
   return (
-    <div className="App">
-      <header className="header1">
+    <div>
+      <div className="header1">
         <p className="text1">E-BIKE Automation</p>
-      </header>
-      <div className="App-header">
-        <p>e-bike automation</p>
-        <QRCode
-          id="qr-gen"
-          value="https://ebike-automate.netlify.app/"
-          size={100}
-          level={"H"}
-          includeMargin={true}
-          style={{marginRight: 50}}
-        />
-        <button type="button" onClick={downloadQRCode}>
-          Download QR Code
-        </button>
-        <RWebShare
-          data={{
-            text: "Web Share - GfG",
-            url: "https://ebike-automate.netlify.app/",
-            title: "GfG",
-          }}
-          onClick={() => console.log("shared successfully!")}
-        >
-          <button>Share on Web</button>
-        </RWebShare>
-        <p>scan or download to share this e-bike website</p>
       </div>
-      <div className="App-header">
-        <p>e-bike automation</p>
-        <QRCode
-          id="qr-gen"
-          value="https://ebike-automate.netlify.app/"
-          size={100}
-          level={"H"}
-          includeMargin={true}
-          style={{marginRight: 50}}
-        />
-        <button type="button" onClick={downloadQRCode}>
-          Download QR Code
-        </button>
-        <p>scan or download to share this e-bike website</p>
-      </div>
-      <div className="App-header">
-        <p>e-bike automation</p>
-        <div className="image_container">
+      <div className="App">
+        <div className="App-header">
+          <p>e-bike automation</p>
+          <QRCode
+            id="qr-gen"
+            value="https://ebike-automate.netlify.app/"
+            size={100}
+            level={"H"}
+            includeMargin={true}
+            style={{marginRight: 50}}
+          />
+          <button type="button" onClick={downloadQRCode}>
+            Download QR Code
+          </button>
+          <RWebShare
+            data={{
+              text: "Web Share - GfG",
+              url: "https://ebike-automate.netlify.app/",
+              title: "GfG",
+            }}
+            onClick={() => console.log("shared successfully!")}
+          >
+            <button>Share on Web</button>
+          </RWebShare>
+          <p>scan or download to share this e-bike website</p>
+        </div>
+        <div className="App-header">
+          <p>e-bike automation</p>
+          <QRCode
+            id="qr-gen"
+            value="https://ebike-automate.netlify.app/"
+            size={100}
+            level={"H"}
+            includeMargin={true}
+            style={{marginRight: 50}}
+          />
+          <button type="button" onClick={downloadQRCode}>
+            Download QR Code
+          </button>
+          <p>scan or download to share this e-bike website</p>
+        </div>
+        <div className="App-header">
+          <p>e-bike automation</p>
+          <div className="image_container">
+            <QRCode
+              id="qr-gen"
+              size={100}
+              level={"H"}
+              includeMargin={true}
+              value="https://ebike-automate.netlify.app/"
+              style={{marginRight: 50}}
+            />
+          </div>
+          <button type="button" onClick={downloadQRCode}>
+            Download QR Code
+          </button>
+          <p>scan or download to share this e-bike website</p>
+        </div>
+        <div className="App-header">
+          <p>e-bike automation</p>
           <QRCode
             id="qr-gen"
             size={100}
@@ -74,44 +91,29 @@ function Layout() {
             value="https://ebike-automate.netlify.app/"
             style={{marginRight: 50}}
           />
+          <button type="button" onClick={downloadQRCode}>
+            Download QR Code
+          </button>
+          <p>scan or download to share this e-bike website</p>
         </div>
-        <button type="button" onClick={downloadQRCode}>
-          Download QR Code
-        </button>
-        <p>scan or download to share this e-bike website</p>
-      </div>
-      <div className="App-header">
-        <p>e-bike automation</p>
-        <QRCode
-          id="qr-gen"
-          size={100}
-          level={"H"}
-          includeMargin={true}
-          value="https://ebike-automate.netlify.app/"
-          style={{marginRight: 50}}
-        />
-        <button type="button" onClick={downloadQRCode}>
-          Download QR Code
-        </button>
-        <p>scan or download to share this e-bike website</p>
-      </div>
-      <div className="App-header">
-        <p>e-bike automation</p>
-        <QRCode
-          id="qr-gen"
-          size={100}
-          level={"H"}
-          includeMargin={true}
-          value="https://ebike-automate.netlify.app/"
-          style={{marginRight: 50}}
-        />
-        <button type="button" onClick={downloadQRCode}>
-          Download QR Code
-        </button>
-        <p>scan or download to share this e-bike website</p>
-      </div>
-      <div className="footer1">
-        <p className="text1">E-Bike Automation</p>
+        <div className="App-header">
+          <p>e-bike automation</p>
+          <QRCode
+            id="qr-gen"
+            size={100}
+            level={"H"}
+            includeMargin={true}
+            value="https://ebike-automate.netlify.app/"
+            style={{marginRight: 50}}
+          />
+          <button type="button" onClick={downloadQRCode}>
+            Download QR Code
+          </button>
+          <p>scan or download to share this e-bike website</p>
+        </div>
+        <div className="footer1">
+          <p className="text1">E-Bike Automation</p>
+        </div>
       </div>
     </div>
   );
