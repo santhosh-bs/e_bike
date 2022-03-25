0fa5c866d8b1700faf6c89c326ad09b8c7037f60
bsanty-santhosh <89247020+bsanty-santhosh@users.noreply.github.com>
2022-03-25 12:34:50 +0530

    layout_construction

---
+++ src/HomePage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Layout from "./Layout";
+
+function HomePage() {
+  return (
+    <Layout>
+      <div className="container1">
+        <div className="section1">
+          <div className="content1">
+            <p className="text1">Mahindra</p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default HomePage;
--- src/Layout.js
+++ src/Layout.js
@@ -3,21 +3,92 @@ import React from "react";
 // import {useRoute} from "@react-navigation/native";
 // import {useLocation} from "react-router-dom";
 import QRCode from "qrcode.react";
+import "./HomePage.css";
 
 function Layout() {
   // const route = useLocation();
   // const route = useRoute();
   return (
     <div className="App">
-      <header className="App-header">
-        <p>e-bike automation</p>
+      <header className="header1">
+        <p className="text1">E-BIKE Automation</p>
       </header>
-      <div>
+      <div className="App-header">
+        <p>e-bike automation</p>
         <QRCode
           value="https://ebike-automate.netlify.app/"
           style={{marginRight: 50}}
         />
         <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <QRCode
+          value="https://ebike-automate.netlify.app/"
+          style={{marginRight: 50}}
+        />
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <div className="image_container">
+          <QRCode
+            value="https://ebike-automate.netlify.app/"
+            style={{marginRight: 50}}
+          />
+        </div>
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <QRCode
+          value="https://ebike-automate.netlify.app/"
+          style={{marginRight: 50}}
+        />
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <QRCode
+          value="https://ebike-automate.netlify.app/"
+          style={{marginRight: 50}}
+        />
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <QRCode
+          value="https://ebike-automate.netlify.app/"
+          style={{marginRight: 50}}
+        />
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <QRCode
+          value="https://ebike-automate.netlify.app/"
+          style={{marginRight: 50}}
+        />
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <QRCode
+          value="https://ebike-automate.netlify.app/"
+          style={{marginRight: 50}}
+        />
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="App-header">
+        <p>e-bike automation</p>
+        <QRCode
+          value="https://ebike-automate.netlify.app/"
+          style={{marginRight: 50}}
+        />
+        <p>scan or download to share this e-bike website</p>
+      </div>
+      <div className="footer1">
+        <p className="text1">E-Bike Automation</p>
       </div>
     </div>
   );
